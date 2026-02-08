01cfe2dc0d4831a60e47955a85eee91e3c9b2a3b
Mark <123863080+marlef03@users.noreply.github.com>
2026-02-08 17:24:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+TOKEN = getenv('TOKEN')
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_command_handler(message: Message):
+    await message.answer('Привет! Это история советсткой и российской вычислительной техники! Рады тебя видеть! :)')
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    
+    await dp.start_polling(bot) 
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
